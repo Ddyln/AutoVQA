cfde333476b8bfe10e10b477d7328d82a73e507f
TranDacThinh <tdthinh0904@gmail.com>
2025-12-16 00:11:43 +0700

    Updating pipeline api call for preventing trigger runtime dependency imports.

--- src/autovqa/__init__.py
+++ src/autovqa/__init__.py
@@ -1,6 +1,31 @@
-from .balancer.balancer_pipeline import balancer_pipeline
-from .exploratory_data_analysis.eda_pipeline import eda_pipeline
-from .filter.filter_pipeline import filter_pipeline
+import importlib
+
 from .version import __version__
 
-__all__ = ["__version__", "eda_pipeline", "filter_pipeline", "balancer_pipeline"]
+__all__ = [
+    "__version__",
+    "eda_pipeline",
+    "filter_pipeline",
+    "balancer_pipeline",
+]
+
+
+def _get_attr(module_name: str, attr: str):
+    mod = importlib.import_module(module_name)
+    return getattr(mod, attr)
+
+
+def eda_pipeline(*args, **kwargs):
+    return _get_attr("autovqa.eda.eda_pipeline", "eda_pipeline")(*args, **kwargs)
+
+
+def filter_pipeline(*args, **kwargs):
+    return _get_attr("autovqa.filter.filter_pipeline", "filter_pipeline")(
+        *args, **kwargs
+    )
+
+
+def balancer_pipeline(*args, **kwargs):
+    return _get_attr("autovqa.balancer.balancer_pipeline", "balancer_pipeline")(
+        *args, **kwargs
+    )
